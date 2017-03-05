eb5e7270cc81a47cde9aa9d71c33b75d4bd93930
Chris Brown <chris@2pha.com>
2017-03-05 12:14:08 +1000

    Added sass.

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,26 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var minifyInline = require('gulp-minify-inline');
 var htmlclean = require('gulp-htmlclean');
+var sass = require('gulp-sass');
 
 //script paths
 var jsFiles = ['src/js/three.js', 'src/js/app.js', 'src/js/shaders/*.js'],
     jsDest = 'build';
+
+var sassFile = 'src/sass/styles.scss',
+    cssDest = 'build';
 
 gulp.task('scripts', function() {
   console.log('doing scripts');
   return gulp.src(jsFiles)
     .pipe(concat('script.js'))
     .pipe(uglify().on('error', console.log))
+    .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('styles', function(){
+  return gulp.src(sassFile)
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(jsDest));
 });
 
@@ -25,4 +35,4 @@ gulp.task('index', function(){
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['index','scripts']);
+gulp.task('default', ['index', 'styles','scripts']);
