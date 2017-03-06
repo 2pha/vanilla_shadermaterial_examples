7f7a4a10e391efe740e261db8c4e5e2f83545103
Chris Brown <chris@2pha.com>
2017-03-06 18:35:30 +1000

    Added cylinder and cone

---
+++ src/js/shapes/cone.js
@@ -0,0 +1,5 @@
+App.shapes.push({
+  name : 'Cone',
+  geo : new THREE.ConeGeometry(100, 200, 32),
+  buttonImage : 'cone.png',
+});
---
+++ src/js/shapes/cylinder.js
@@ -0,0 +1,5 @@
+App.shapes.push({
+  name : 'Cylinder',
+  geo : new THREE.CylinderGeometry(100, 100, 200, 32),
+  buttonImage : 'cylinder.png',
+});
